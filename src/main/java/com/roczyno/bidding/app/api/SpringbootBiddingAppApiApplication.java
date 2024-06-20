0e436a291e805818434aa03707147f5f51f58dc2
roczyno <adiabaroczyno9@gmail.com>
2024-06-20 09:25:15 +0000

    starting project

---
+++ src/main/java/com/roczyno/bidding/app/api/SpringbootBiddingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.roczyno.bidding.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBiddingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBiddingAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roczyno/bidding/app/api/SpringbootBiddingAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roczyno.bidding.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBiddingAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
