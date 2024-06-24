c1a65502f39a084882578bed08956c64e6f7bccb
roczyno <adiabaroczyno9@gmail.com>
2024-06-24 16:25:11 +0000

    add: implement create subscription

--- src/main/java/com/roczyno/bidding/app/api/service/SubscriptionServiceImpl.java
+++ src/main/java/com/roczyno/bidding/app/api/service/SubscriptionServiceImpl.java
@@ -3,23 +3,27 @@ package com.roczyno.bidding.app.api.service;
 import com.roczyno.bidding.app.api.model.PlanType;
 import com.roczyno.bidding.app.api.model.Subscription;
 import com.roczyno.bidding.app.api.model.User;
+import com.roczyno.bidding.app.api.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
+@RequiredArgsConstructor
 public class SubscriptionServiceImpl  implements SubscriptionService{
+	private final SubscriptionRepository subscriptionRepository;
 	@Override
 	public Subscription createSubscription(User user) {
 
-		Subscription.builder()
+		var subscription =Subscription.builder()
 				.user(user)
 				.subscriptionStartDate(LocalDate.now())
 				.subscriptionEndDate(LocalDate.now().plusDays(12))
 				.planType(PlanType.BASIC)
 				.build();
-		return null;
+		return subscriptionRepository.save(subscription);
 	}
 
 	@Override
